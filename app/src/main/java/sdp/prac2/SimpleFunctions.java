19b18a6df993193c583e9720dcf8895b56d85374
Litha <mphakzmabona@gmail.com>
2022-07-21 15:56:14 +0200

    task2-Litha

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -2,6 +2,21 @@ package sdp.prac2;
 
 import java.util.*;
 
+import org.checkerframework.checker.index.qual.SubstringIndexBottom;
+
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+
+    }
+
+    public List<String> task2(List<String> list) {
+        for (int i = 0;i < list.size();i++){
+            if (list.get(i).isEmpty()) { list.remove(i); } 
+            else { 
+                String item = list.get(i).substring(1,-1);
+                list.set(i,item);
+            }
+        }   
+        return list;
+    }
 }
