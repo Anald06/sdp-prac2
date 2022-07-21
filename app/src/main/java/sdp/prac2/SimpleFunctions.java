db517a00797c7640e889be6cbd291a1697a3ce80
Ahmed Kobue <g19k0734@campus.ru.ac.za>
2022-07-21 15:01:25 +0200

    A  Method which returns true only if the number of left parenthesis is equivalent to the number of right parenthesis

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,21 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+   
+        static boolean Task3(String str) {
+            int left = 0;
+            int right = 0;
+            char[] chars = str.toCharArray();
+            for(int i =0 ;i<chars.length;i++){
+                if(chars[i]=='('){
+                    left++;
+                }
+                else if(chars[i]==')'){
+                    right++;
+                }
+                
+            }
+            return left==right;
+        }
 }
+
