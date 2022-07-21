80c68953bdc9f5d1e390221da94c10a64d4a35a4
Mduduzi Ndlovu <Mduduzindlovu02@gmail.com>
2022-07-21 16:53:24 +0200

    task6

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,25 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    // task 6 begins here 
+    public List<Integer> Task6(List<Integer> old_List) {
+        // new list here 
+        List<Integer> new_List = new ArrayList<Integer>();
+    
+        for(int index = 0 ; index < old_List.size(); index++)
+        {
+            //old_List.add(index)
+             int multi = (((int)(Math.ceil(old_List.get(index))) + 99) / 100) * 100;
+             //new_List.add(old_list[count]); 
+             new_List.add(multi);
+        }
+        return new_List;
+    };
+    
 }
+
+
+
+
+
