5efb4583f88e52bec70bf061a1e277f2e3bafe7f
ElishmaM <109703232+ElishmaM@users.noreply.github.com>
2022-07-25 22:38:13 +0200

    This is Task 5

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,26 +3,11 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
 
-    // task 6 begins here 
-    public List<Integer> Task6(List<Integer> old_List) {
-        // new list here 
-        List<Integer> new_List = new ArrayList<Integer>();
-    
-        for(int index = 0 ; index < old_List.size(); index++)
-        {
-            //old_List.add(index)
-             int multi = (((int)(Math.ceil(old_List.get(index))) + 99) / 100) * 100;
-             //new_List.add(old_list[count]); 
-             new_List.add(multi);
-        }
-        return new_List;
-    };
-    
+    public static boolean Task5(List<Integer> lst) {
+        List<Integer> temp = new ArrayList<>(lst);
+        java.util.Collections.sort(lst);
+        return temp.equals(lst);
+    }
+
 }
-
-
-
-
-
